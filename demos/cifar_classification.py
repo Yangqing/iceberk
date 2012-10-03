e90a723b18631b58d02fc792724ddaa44a29b09f
Yangqing Jia <jiayq@eecs.berkeley.edu>
2012-10-03 10:43:56 -0700

    cifar_classification.py now works as a demo script to run classification on cifar.

--- demos/cifar_classification.py
+++ demos/cifar_classification.py
@@ -1,32 +1,44 @@
 '''
-Created on Sep 20, 2012
+This script uses the jiayq_ice pipeline to perform a cifar classification demo
+using parameter settings idential to Adam Coates' AISTATS paper (except for the
+number of kmeans centers, which we set to 800 for speed considerations).
+
+You need to specify "--root=/path/to/cifar-data" to run the code. For other
+optional flags, run the script with --help or --helpshort.
 
 @author: jiayq
 '''
+
 import cPickle as pickle
 import cProfile
 import gflags
 import logging
 from jiayq_ice import mpi, visiondata, pipeline, classifier
 import numpy as np
+import os
 import sys
 
 gflags.DEFINE_string("root", "",
-                     "The root to the cifar dataset")
+                     "The root to the cifar dataset (python format)")
 gflags.RegisterValidator('root', lambda x: x != "",
                          message='--root must be provided.')
 gflags.DEFINE_string("output_dir", ".",
-                     "The output directory")
+                     "The output directory that stores dumped feature and"
+                     "classifier files.")
 gflags.DEFINE_string("model_file", "conv.pickle",
-                     "The output model file")
+                     "The filename to output the CNN model.")
 gflags.DEFINE_string("feature_file", "cnn_features",
-                     "The output feature files")
+                     "The filename to output the features.")
 gflags.DEFINE_string("svm_file", "svm.pickle",
-                     "The output svm file")
-gflags.DEFINE_string("profile", "",
-                     "If set, do cProfile analysis")
+                     "The filename to output the trained svm parameters.")
+gflags.DEFINE_string("profile_file", "",
+                     "If set, do cProfile analysis, and output the result"
+                     "to the filename given by the flag.")
 FLAGS = gflags.FLAGS
-def demo():
+
+def cifar_demo():
+    """Performs a demo classification on cifar
+    """
     mpi.mkdir(FLAGS.output_dir)
     logging.info('Loading cifar data...')
     cifar = visiondata.CifarDataset(FLAGS.root, is_training=True)
@@ -36,41 +48,46 @@ def demo():
             pipeline.PatchExtractor([6,6], 1), # extracts patches
             pipeline.MeanvarNormalizer({'reg': 10}), # normalizes the patches
             pipeline.LinearEncoder({},
-                    trainer = pipeline.ZcaTrainer({'reg': 0.01})), # Does whitening
+                    trainer = pipeline.ZcaTrainer({'reg': 0.1})), # Does whitening
             pipeline.TriangleEncoder({},
-                    trainer = pipeline.KmeansTrainer({'k': 800})), # does encoding
+                    trainer = pipeline.KmeansTrainer(
+                            {'k': 800, 'max_iter':100})), # does encoding
             pipeline.SpatialPooler({'grid': (2,2), 'method': 'ave'}) # average pool
             ])
     logging.info('Training the pipeline...')
     conv.train(cifar, 400000)
     if mpi.is_root():
-        with open(FLAGS.model_file,'w') as fid:
+        with open(os.path.join(FLAGS.output_dir, FLAGS.model_file),'w') as fid:
             pickle.dump(conv, fid)
             fid.close()
     
     logging.info('Extracting features...')
     Xtrain = conv.process_dataset(cifar, as_2d = True)
-    mpi.dump_matrix_multi(Xtrain, FLAGS.feature_file+'_train')
-    Ytrain = cifar.labels()
+    mpi.dump_matrix_multi(Xtrain,
+                          os.path.join(FLAGS.output_dir, 
+                                       FLAGS.feature_file+'_train'))
+    Ytrain = cifar.labels().astype(np.int)
     Xtest = conv.process_dataset(cifar_test, as_2d = True)
-    mpi.dump_matrix_multi(Xtest, FLAGS.feature_file+'_test')
-    Ytest = cifar_test.labels()
+    mpi.dump_matrix_multi(Xtest,
+                          os.path.join(FLAGS.output_dir, 
+                                       FLAGS.feature_file+'_test'))
+    Ytest = cifar_test.labels().astype(np.int)
     
     # normalization
-    m = Xtrain.mean(axis=1)
-    std = Xtrain.std(asix=1)
+    m, std = classifier.feature_meanstd(Xtrain)
     Xtrain -= m
     Xtrain /= std
     Xtest -= m
     Xtest /= std
     
     w, b = classifier.l2svm_onevsall(Xtrain, Ytrain, 0.01)
-    with open(FLAGS.svm_file, 'w') as fid:
-        pickle.dump({'m': m, 'std': std, 'w': w, 'b': b}, fid)
+    if mpi.is_root():
+        with open(os.path.join(FLAGS.output_dir, FLAGS.svm_file), 'w') as fid:
+            pickle.dump({'m': m, 'std': std, 'w': w, 'b': b}, fid)
     accu = np.sum(Ytrain == (np.dot(Xtrain,w)+b).argmax(axis=1)) \
             / float(len(Ytrain))
-    accu_test = np.sum(Ytrain == (np.dot(Xtrain,w)+b).argmax(axis=1)) \
-            / float(len(Ytrain))
+    accu_test = np.sum(Ytest == (np.dot(Xtest,w)+b).argmax(axis=1)) \
+            / float(len(Ytest))
     
     logging.info('Training accuracy: %f' % accu)
     logging.info('Testing accuracy: %f' % accu_test)
@@ -80,8 +97,8 @@ if __name__ == "__main__":
     if mpi.is_root():
         logging.basicConfig(level=logging.DEBUG)
         if FLAGS.profile != "":
-            cProfile.run('demo()', FLAGS.profile)
+            cProfile.run('cifar_demo()', FLAGS.profile)
         else:
-            demo()
+            cifar_demo()
     else:
-        demo()
+        cifar_demo()
