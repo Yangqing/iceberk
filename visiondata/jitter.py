6f8d1565357c11b70250cf369b1575188ca537e9
Yangqing Jia <jiayq@eecs.berkeley.edu>
2012-10-26 09:55:22 -0700

    the griddata version of jitter, very slow

---
+++ visiondata/jitter.py
@@ -0,0 +1,58 @@
+'''JITTER implements a toy jittering effect for images
+
+@author: jiayq
+'''
+
+import numpy as np
+from scipy import interpolate
+
+def jitter(img, translation, rotation, scaling):
+    """ Jittering an image with the given translation, rotation and scaling
+    """
+    # we write the program in a general fashion assuming that the image is 
+    # multi channel.
+    img = np.atleast_3d(img)
+    img_size = np.array(img.shape[:2])
+    center = img_size / 2. - 0.5
+    hh, ww = np.meshgrid(np.arange(img_size[0]), np.arange(img_size[1]))
+    old_coor = np.hstack((hh.reshape(hh.size, 1), ww.reshape(ww.size, 1)))\
+                - center
+    
+    rotation_matrix = np.asarray([[ np.cos(rotation), -np.sin(rotation)],
+                                  [ np.sin(rotation), np.cos(rotation)]])
+    new_coor = np.dot(old_coor, rotation_matrix)
+    new_coor += translation
+    new_coor *= 2. ** scaling
+    img_jittered = np.empty_like(img)
+    # we use linear interpolation to create the image for better quality, and
+    # use the nearest values for pixels outside the image
+    for i in range(img.shape[2]):
+        values = img[:,:,i].flatten()
+        out = interpolate.griddata(new_coor, values, old_coor);
+        out_near = interpolate.griddata(new_coor, values, old_coor,
+                                        method='nearest')
+        to_fill = np.isnan(out)
+        out[to_fill] = out_near[to_fill]
+        img_jittered[:,:,i] = out.reshape(img_size)
+    # finally, if it's a single channel image, we will just return a single
+    # channel image
+    if img_jittered.shape[2] == 1:
+        img_jittered.reshape(img_size)
+    return img_jittered
+
+def random_jitter(img, translation_std, rotation_std, scaling_std):
+    """Randomly jitter an image by providing the translation, rotation and
+    scaling standard deviations
+    
+    Definition of std:
+        translation: in the number of pixels
+        rotation: in degrees
+        scaling: in log_2 scale, e.g. scaling=0.5 means 2^0.5 times larger
+    """
+    translation = np.random.randn(2) * translation_std
+    rotation = np.random.randn() * np.pi * rotation_std / 180.
+    scaling = np.random.randn() * scaling_std
+    return jitter(img, translation, rotation, scaling)
+
+if __name__ == "_main__":
+    pass
