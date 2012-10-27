fc4543a456e25342fea8dbe0484e616227b10357
Yangqing Jia <jiayq@eecs.berkeley.edu>
2012-10-26 17:07:56 -0700

    working jitter

--- demos/demo_jitter.py
+++ demos/demo_jitter.py
@@ -7,7 +7,7 @@ def demo_jitter():
     filename = os.path.join(os.path.dirname(iceberk.__file__),
                             'test','data','lena.png')
     img = misc.imread(filename)
-    img_out = jitter.randr_jitter(img, 10, 10, 0.2)
+    img_out = jitter.randn_jitter(img, [10, 10, 0.2])
     misc.imsave('demo_jitter_lena.png', img_out)
 
 if __name__ == "__main__":
--- visiondata/jitter.py
+++ visiondata/jitter.py
@@ -3,6 +3,7 @@
 @author: jiayq
 '''
 
+from iceberk import datasets
 import numpy as np
 from scipy import interpolate
 
@@ -39,7 +40,7 @@ def jitter(img, translation, rotation, scaling):
         img_jittered.reshape(img_size)
     return img_jittered
 
-def randr_jitter(img, translation_std, rotation_std, scaling_std):
+def randn_jitter(img, stdval):
     """Randomly jitter an image by providing the translation, rotation and
     scaling standard deviations. The exact parameters are sampled from Gaussian
     distributions.
@@ -49,12 +50,12 @@ def randr_jitter(img, translation_std, rotation_std, scaling_std):
         rotation: in degrees
         scaling: in log_2 scale, e.g. scaling=0.5 means 2^0.5 times larger
     """
-    translation = np.random.randn(2) * translation_std
-    rotation = np.random.randn() * np.pi * rotation_std / 180.
-    scaling = np.random.randn() * scaling_std
+    translation = np.random.randn(2) * stdval[0]
+    rotation = np.random.randn() * np.pi * stdval[1] / 180.
+    scaling = np.random.randn() * stdval[2]
     return jitter(img, translation, rotation, scaling)
 
-def rand_jitter(img, translation_max, rotation_max, scaling_max):
+def rand_jitter(img, maxval):
     """Randomly jitter an image by providing the translation, rotation and
     scaling standard deviations. The exact parameters are sampled from uniform
     distributions bounded by [-max, max].
@@ -64,10 +65,84 @@ def rand_jitter(img, translation_max, rotation_max, scaling_max):
         rotation: in degrees
         scaling: in log_2 scale, e.g. scaling=0.5 means 2^0.5 times larger
     """
-    translation = (np.random.rand(2) * 2. - 1.) * translation_max
-    rotation = (np.random.rand() * 2. - 1.) * np.pi * rotation_max / 180.
-    scaling = (np.random.rand() * 2. - 1.) * scaling_max
+    translation = (np.random.rand(2) * 2. - 1.) * maxval[0]
+    rotation = (np.random.rand() * 2. - 1.) * np.pi * maxval[1] / 180.
+    scaling = (np.random.rand() * 2. - 1.) * maxval[2]
     return jitter(img, translation, rotation, scaling)
+
+
+class JitterSet(datasets.ImageSet):
+    """JitterSet provides a jittered version of the dataset by randomly 
+    adding jitter effect to the images. 
+    """
+    def __init__(self, original_set, factor, params, method = 'uniform'):
+        """Create a mirrored dataset from the original data set.
+        Input:
+            original_set: the original dataset
+            factor: a positive number. the dataset is expanded 
+                factor times - for each image we will have the original image
+                and factor jittered images. Note that the parameters of the 
+                jittered images are randomly regenerated every time you call
+                image, so if you call image(n) two times with the same n, the
+                results will still be different - this is by design since it
+                makes stochastic approaches easier.
+                
+                If factor = 0, the original images are not returned, and all 
+                images will be jittered.
+            params: the 3-dimensional array containing the parameters for 
+                jittering
+            method: either 'uniform' or 'gaussian'.
+        """
+        super(JitterSet, self).__init__()
+        self._original = original_set
+        self._factor = factor
+        self._params = np.asarray(params)
+        if method == 'uniform':
+            self._jitter = rand_jitter
+        elif method == 'gaussian':
+            self._jitter = randn_jitter
+        else:
+            raise ValueError, "Unrecognized method name: %s" % (method)
+        
+
+    def size(self):
+        """Return the size of the dataset hosted on the current node
+        """
+        return self._original.size() * (self._factor + 1)
+
+    def image(self, idx):
+        """ Returns datum 
+        
+        Note that you should almost never use data that is hosted on other
+        nodes - every node should deal with its data only.
+        """
+        if self._factor > 0 and idx < self._original.size():
+            return self._original.image(idx)
+        else:
+            im = self._original.image(idx % self._original.size())
+            return self._jitter(im, self._params)
+
+    def label(self, idx):
+        """ Returns the label for the corresponding datum
+        """
+        return self._original.label(idx % self._original.size())
+
+    def labels(self):
+        """ Returns the label vector for all the data I am hosting
+        """
+        return np.hstack([self._original.labels() 
+                          for _ in range(self._factor + 1)])
+    
+    def dim(self):
+        """Returns the dimension of the data if they have the same dimension
+        Otherwise, return False
+        """
+        return self._original.dim()
+    
+    def num_channels(self):
+        """ Returns the number of channels
+        """
+        return self._original.num_channels()
 
 if __name__ == "_main__":
     pass
