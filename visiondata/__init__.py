f1e492b5243dfcade4d1258cb6147efc9055f383
Yangqing Jia <jiayq84@gmail.com>
2012-11-20 15:25:47 -0800

    misc updates to the datasets

--- visiondata/__init__.py
+++ visiondata/__init__.py
@@ -4,6 +4,7 @@ format defined in iceberk.datasets
 
 from iceberk.visiondata._cifar import CifarDataset
 from iceberk.visiondata._cub import CUBDataset
+from iceberk.visiondata._stanford_dog import StanfordDogDataset
 from iceberk.visiondata._mnist import MNISTDataset
 from iceberk.visiondata._stl_10 import STL10Dataset
 from iceberk.visiondata.jitter import JitterSet
