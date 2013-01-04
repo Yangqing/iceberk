ecc3f1c896a7f74263f934cc0c3dfef3190f1ee3
Yangqing Jia <jiayq84@gmail.com>
2013-01-04 14:26:12 -0800

    h5py error -> warning

--- visiondata/__init__.py
+++ visiondata/__init__.py
@@ -2,12 +2,18 @@
 format defined in iceberk.datasets
 """
 
+import logging
 
 from iceberk.visiondata._cifar import CifarDataset
 from iceberk.visiondata._cub import CUBDataset
 from iceberk.visiondata._stanford_dog import StanfordDogDataset
 from iceberk.visiondata._mnist import MNISTDataset
-from iceberk.visiondata._stl_10 import STL10Dataset
+try:
+    from iceberk.visiondata._stl_10 import STL10Dataset
+except ImportError, e:
+    logging.warning("Warning: The code for STL-10 dataset needs the h5py package, "\
+                    "which is not available.")
+    STL10Dataset = None
 from iceberk.visiondata.jitter import JitterSet
 from iceberk.datasets import TwoLayerDataset
 
