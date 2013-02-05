941f10289bda4267d8c0365cd977d1b954de2cc1
Yangqing Jia <jiayq84@gmail.com>
2013-02-04 16:33:03 -0800

    classifier: elapsted time should be total() instead of lap()

--- classifier.py
+++ classifier.py
@@ -295,7 +295,7 @@ class SolverStochastic(Solver):
         timer = util.Timer()
         for iter in range(self._args['num_iter']):
             logging.info('Solver: running lbfgs round %d, elapsed %s' % \
-                    (iter, timer.lap()))
+                    (iter, timer.total()))
             if (X.shape[0] - pointer < minibatch):
                 # reshuffle
                 SolverStochastic.synchronized_shuffle((X, Y, weight))
